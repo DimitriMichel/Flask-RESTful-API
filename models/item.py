b181b21c3c44787ca4c32953f25a17e48eb63aec
Dimitri Michel <54259068+DimitriMichel@users.noreply.github.com>
2019-10-14 19:39:14 -0400

    Add files via upload

--- models/item.py
+++ models/item.py
@@ -1,5 +1,4 @@
 from db import db
-# creating an item model
 
 
 class ItemModel(db.Model):
@@ -12,20 +11,26 @@ class ItemModel(db.Model):
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     store = db.relationship('StoreModel')
 
-    # must match previously defined database columns above
     def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'store_id': self.store_id
+        }
 
     @classmethod
     def find_by_name(cls, name):
-        # this return is 'SELECT * FROM items WHERE name=name LIMIT 1'
-        return ItemModel.query.filter_by(name=name).first()  # .query comes from sqlalchemy
-        # this returns an item model object that contains self.name and self.price
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
@@ -34,4 +39,3 @@ class ItemModel(db.Model):
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-
--- models/user.py
+++ models/user.py
@@ -1,56 +1,36 @@
-import sqlite3
 from db import db
-# moved into model folder because the api cannot receive data or send JSON from this class
-# these methods below allow us to receive user help us store information about a user
-# also these methods allow us to retrieve user objects from a database
 
 
 class UserModel(db.Model):
-    # here we tell sqlalchemy the columns which our model will have and it will look for these three properties.
-    __tablename__ = 'items'
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    # in this function def we must relate them to the database they must match the data columns above or will not save
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-# here we set methods to retrieve the username and password from a user in our database
+
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
 
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-# after this declaration all cursor methods are bound to the declared database of 'data.db'
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))  # must be a tuple in order to have a tuple we need the comma(x,)
-        row = result.fetchone()
-# our row = result.fetchone just means row will equal the first username found
-        if row:
-            user = cls(*row)    # (*args) recall * is all and args is arguments this code fills with database info
-        else:                   # cls(*row) fills in with our CLASS arguments of (_id, username, password)
-            user = None
-
-        connection.close()      # this line tells us the cursor will be unusable from this point forward
-        return user
-# here we define a class function that will find our user's username from a SQL database
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(id=_id).first()
 
-        query = "SELECT * FROM {table} WHERE id=?".format(table=cls.TABLE_NAME)
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-        connection.close()
-        return user
-# here we define a class function that will find our user's id from a SQL database
-# otherwise the same things are happening here
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
