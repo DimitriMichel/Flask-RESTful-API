af99d1627d383d9a5c9b8baa94b0ed3a86b7067f
Dimitri Michel <54259068+DimitriMichel@users.noreply.github.com>
2019-10-15 05:16:53 -0400

    Add files via upload

--- models/store.py
+++ models/store.py
@@ -4,28 +4,20 @@ from db import db
 class StoreModel(db.Model):
     __tablename__ = 'stores'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # unique store id created automatically
     name = db.Column(db.String(80))
 
-    items = db.relationship('ItemModel', lazy='dynamic')
+    items = db.relationship('ItemModel', lazy='dynamic')  # load the ItemModel class from which to derive 'items'
 
     def __init__(self, name):
         self.name = name
 
     def json(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'items': [item.json() for item in self.items.all()]
-        }
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
-
-    @classmethod
-    def find_all(cls):
-        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
@@ -34,4 +26,3 @@ class StoreModel(db.Model):
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-
